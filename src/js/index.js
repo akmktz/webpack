e61749f97bb49328410eb4c544f6f6ffafff837d
akmktz <akmktz@users.noreply.github.com>
2018-10-19 16:05:41 +0300

    JQuery $ fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,4 @@
 import jQuery from 'jquery';
+window.$ = jQuery;
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
-
-jQuery(function() {
-    jQuery('body').css('color', 'blue');
-});
